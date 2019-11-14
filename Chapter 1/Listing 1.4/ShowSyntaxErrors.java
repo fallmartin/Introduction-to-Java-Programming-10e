f6385fa21845d5e8c643fc5264fc66c750c58149
fallmartin <martinfall@gmail.com>
2019-11-14 12:34:30 -0500

    Create ShowSyntaxErrors.java

---
+++ Chapter 1/Listing 1.4/ShowSyntaxErrors.java
@@ -0,0 +1,7 @@
+public class ShowSyntaxErrors {
+    // public static main(String[] args) {
+    public static void main(String[] args) {
+        // System.out.println("Welcome to Java);
+        System.out.println("Welcome to Java");
+    }
+}
