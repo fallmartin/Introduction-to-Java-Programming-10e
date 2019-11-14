4244f74eca346674d6671fbe9370f3f201b31e46
fallmartin <martinfall@gmail.com>
2019-11-14 12:37:20 -0500

    Update ShowRuntimeErrors.java

--- Chapter 1/Listing 1.5/ShowRuntimeErrors.java
+++ Chapter 1/Listing 1.5/ShowRuntimeErrors.java
@@ -1,5 +1,6 @@
 public class ShowRuntimeErrors {
     public static void main(String[] args) {
+        // Will throw an exception in thread "main" java.lang.ArithmeticException: / by zero
         System.out.println(1 / 0);
     }
 }
