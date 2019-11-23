4eb3ed9dfbeb24ba53ebf9b0c2e085fc3633592a
fallmartin <martinfall@gmail.com>
2019-11-23 15:21:52 -0500

    Create Exercise19.java

---
+++ Chapter 1/Programming Exercise 1.9/Exercise19.java
@@ -0,0 +1,6 @@
+public class Exercise19 {
+    public static void main(String[] args) {
+        System.out.println("Area = " + 4.5 * 7.9);
+        System.out.println("Perimeter = " + 2 * (4.5 + 7.9) );
+    }
+}
