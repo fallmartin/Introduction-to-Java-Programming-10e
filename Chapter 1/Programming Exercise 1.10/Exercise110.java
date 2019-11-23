8f30c56ca32c6da290448479761e1bc569c21e00
fallmartin <martinfall@gmail.com>
2019-11-23 15:22:28 -0500

    Create Exercise110.java

---
+++ Chapter 1/Programming Exercise 1.10/Exercise110.java
@@ -0,0 +1,11 @@
+public class Exercise110 {
+    public static void main(String[] args) {
+        // Average speed in miles
+        double km = 14.0;
+        double mi = km * 0.621;
+        
+        double h = 45.5 / 60;
+        
+        System.out.println(mi / h);
+    }
+}
