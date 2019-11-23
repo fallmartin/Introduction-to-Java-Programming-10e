8e2832e187fb828b759f43f58ce4610852befede
fallmartin <martinfall@gmail.com>
2019-11-23 15:17:05 -0500

    Update Exercise12.java

--- Chapter 1/Programming Exercise 1.2/Exercise12.java
+++ Chapter 1/Programming Exercise 1.2/Exercise12.java
@@ -1,1 +1,7 @@
-
+public class Exercise12 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java");
+        }
+    }
+}
