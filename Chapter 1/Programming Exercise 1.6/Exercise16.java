3dc41078876dbbebad3164c622fc63cf6e2c4fb3
fallmartin <martinfall@gmail.com>
2019-11-23 15:19:48 -0500

    Create Exercise16.java

---
+++ Chapter 1/Programming Exercise 1.6/Exercise16.java
@@ -0,0 +1,15 @@
+public class Exercise16 {
+    public static void main(String[] args) {
+        // Solve the problem using a for loop
+        int sum = 0;
+        
+        for (int i = 1; i <=9; i++) {
+            sum += i;
+        }
+        
+        System.out.println(sum);
+        
+        // Solve the problem directly
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    }
+}
