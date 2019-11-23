040a0de1f3523b1f2da2e4104b424e983c4ddedf
fallmartin <martinfall@gmail.com>
2019-11-23 15:21:18 -0500

    Create Exercise18.java

---
+++ Chapter 1/Programming Exercise 1.8/Exercise18.java
@@ -0,0 +1,6 @@
+public class Exercise18 {
+    public static void main(String[] args) {
+        System.out.println(2 * 5.5 * Math.PI);
+        System.out.println(Math.pow(5.5, 2) * Math.PI);
+    }
+}
