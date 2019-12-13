c03a16ec835ddaf74857c20b7fdf3bf7ed15bde0
fallmartin <martinfall@gmail.com>
2019-12-12 23:39:58 -0500

    Create RandomMonth.java

---
+++ Chapter 3/Programming Exercise 4/RandomMonth.java
@@ -0,0 +1,58 @@
+
+/**
+ * (Random month) Write a program that randomly generates an integer between 1 and
+ * 12 and displays the English month name January, February, …, December for the
+ * number 1, 2, …, 12, accordingly.
+ */
+/**
+ *
+ * @author martinfall
+ */
+public class RandomMonth {
+
+    public static void main(String[] args) {
+        int number = (int) ((Math.random() * 12) + 1);
+
+        switch (number) {
+            case 1:
+                System.out.println("January");
+                break;
+            case 2:
+                System.out.println("February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println("May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11:
+                System.out.println("November");
+                break;
+            case 12:
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("Not a valid month");
+                break;
+        }
+    }
+}
