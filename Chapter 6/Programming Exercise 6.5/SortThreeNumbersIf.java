5f5eb24109b5b30e8700faeecd93651b2fc9e839
fallmartin <martinfall@gmail.com>
2020-04-21 02:16:56 -0400

    Update SortThreeNumbersIf.java

--- Chapter 6/Programming Exercise 6.5/SortThreeNumbersIf.java
+++ Chapter 6/Programming Exercise 6.5/SortThreeNumbersIf.java
@@ -2,13 +2,13 @@
 import java.util.Scanner;
 
 /**
- * ﻿(Sort three numbers) Write a method with the following header to display
+ * (Sort three numbers) Write a method with the following header to display
  * three numbers in increasing order:
  *
  * public static void displaySortedNumbers(double num1, double num2, double
  * num3)
  *
- * ﻿Write a test program that prompts the user to enter three numbers and
+ * Write a test program that prompts the user to enter three numbers and
  * invokes the method to display them in increasing order.
  */
 /**
@@ -46,28 +46,22 @@ public class SortThreeNumbersIf {
     public static void displaySortedNumbers(double num1, double num2, double num3) {
         if (num1 < num2) {
             if (num2 < num3) {
-                System.out.println("abc");
                 System.out.println(num1 + " " + num2 + " " + num3);
             } else {
                 if (num1 < num3) {
-                    System.out.println("acb");
                     System.out.println(num1 + " " + num3 + " " + num2);
                 } else {
-                    System.out.println("cab");
                     System.out.println(num3 + " " + num1 + " " + num2);
                 }
             }
         } else {
             if (num2 < num3) {
                 if (num1 < num3) {
-                    System.out.println("bac");
                     System.out.println(num2 + " " + num1 + " " + num3);
                 } else {
-                    System.out.println("bca");
                     System.out.println(num2 + " " + num3 + " " + num1);
                 }
             } else {
-                System.out.println("cba");
                 System.out.println(num3 + " " + num2 + " " + num1);
             }
         }
