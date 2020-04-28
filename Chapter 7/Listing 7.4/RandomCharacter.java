78e1965f6f069c3f084926b3b06b6c3dadd6a650
fallmartin <martinfall@gmail.com>
2020-04-28 02:04:48 -0400

    Create RandomCharacter.java

---
+++ Chapter 7/Listing 7.4/RandomCharacter.java
@@ -0,0 +1,57 @@
+/**
+ * Output:
+ *
+ */
+/**
+ *
+ * @author martinfall
+ */
+public class RandomCharacter {
+
+    /**
+     * Generate a random character between ch1 and ch2
+     *
+     * @param ch1
+     * @param ch2
+     * @return
+     */
+    public static char getRandomCharacter(char ch1, char ch2) {
+        return (char) (ch1 + Math.random() * (ch2 - ch1 + 1));
+    }
+
+    /**
+     * Generate a random lowercase letter
+     *
+     * @return
+     */
+    public static char getRandomLowerCaseLetter() {
+        return getRandomCharacter('a', 'z');
+    }
+
+    /**
+     * Generate a random uppercase letter
+     *
+     * @return
+     */
+    public static char getRandomUpperCaseLetter() {
+        return getRandomCharacter('A', 'Z');
+    }
+
+    /**
+     * Generate a random digit character
+     *
+     * @return
+     */
+    public static char getRandomDigitCharacter() {
+        return getRandomCharacter('0', '9');
+    }
+
+    /**
+     * Generate a random uppercase letter
+     *
+     * @return
+     */
+    public static char getRandomCharacter() {
+        return getRandomCharacter('\u0000', '\uFFFF');
+    }
+}
