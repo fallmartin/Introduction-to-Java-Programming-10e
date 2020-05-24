56751be46726e4ba84f2d35e99d01d67c000295a
fallmartin <martinfall@gmail.com>
2020-05-24 03:21:34 -0400

    Create GradeExam.java

---
+++ Chapter 8/Listing 8.2/GradeExam.java
@@ -0,0 +1,46 @@
+
+/**
+ * Output:
+ *
+ * Student 0's correct count is 7
+ * Student 1's correct count is 6
+ * Student 2's correct count is 5
+ * Student 3's correct count is 4
+ * Student 4's correct count is 8
+ * Student 5's correct count is 7
+ * Student 6's correct count is 7
+ * Student 7's correct count is 7
+ *
+ * @author martinfall
+ */
+public class GradeExam {
+
+    public static void main(String[] args) {
+        // Students' answers to the questions
+        char[][] answers = {
+            {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+            {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+            {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+            {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+            {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+            {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+            {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+            {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
+
+        // Key to the questions
+        char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+
+        // Grade all answers
+        for (int i = 0; i < answers.length; i++) {
+            // Grade one student
+            int correctCount = 0;
+            for (int j = 0; j < answers[i].length; j++) {
+                if (answers[i][j] == keys[j]) {
+                    correctCount++;
+                }
+            }
+            System.out.println("Student " + i + "'s correct count is "
+                    + correctCount);
+        }
+    }
+}
