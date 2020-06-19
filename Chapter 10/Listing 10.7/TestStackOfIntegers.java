26c431634722513657072223ccb44e22371e2a74
fallmartin <martinfall@gmail.com>
2020-06-19 02:33:41 -0400

    Create TestStackOfIntegers.java

---
+++ Chapter 10/Listing 10.7/TestStackOfIntegers.java
@@ -0,0 +1,18 @@
+
+/**
+ *
+ * @author martinfall
+ */
+public class TestStackOfIntegers {
+    public static void main(String[] args) {
+        StackOfIntegers stack = new StackOfIntegers();
+        
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        
+        while (!stack.empty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
