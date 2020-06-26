de39864d6c6ec776d83b7626a9aa5b9e97d14e0c
fallmartin <martinfall@gmail.com>
2020-06-26 03:45:47 -0400

    Update findPrime.java

--- Chapter 10/Programming Exercise 10.18/findPrime.java
+++ Chapter 10/Programming Exercise 10.18/findPrime.java
@@ -42,6 +42,7 @@ public class findPrime {
         // for (int divisor = 2; divisor < b / 2; divisor++)
         for (BigInteger divisor = new BigInteger("2");
                 divisor.compareTo(b.divide(new BigInteger("2"))) == -1;
+             // I may need to add || divisor.compareTo(b.divide(new BigInteger("2"))) == 0 to include the uppderlimit b / 2
                 divisor = divisor.add(BigInteger.ONE)) {
             if (b.divide(divisor).equals(BigInteger.ZERO)) {
                 return false;
