73fb386c75e1aa1fd84a8cc831191ab12adf3df8
fallmartin <martinfall@gmail.com>
2020-07-09 00:26:14 -0400

    Create QuotientWithIf.java

---
+++ Chapter 12/Listing 12.2/QuotientWithIf.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+/**
+ * Output:
+ * Enter two integers: 5 0
+ * Divisor cannot be zero
+ *
+ * @author martinfall
+ */
+public class QuotientWithIf {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter two integers
+        System.out.print("Enter two integers: ");
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+
+        if (number2 != 0) {
+            System.out.println(number1 + " / " + number2 + " is " + (number1 / number2));
+        } else {
+            System.out.println("Divisor cannot be zero");
+        }
+    }
+}
