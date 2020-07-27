7887e8e23f9630cd0b6badec4570bbc1c7527436
fallmartin <martinfall@gmail.com>
2020-07-27 03:06:36 -0400

    Create GeometricObject.java

---
+++ Chapter 13/Programming Exercise 13.7/GeometricObject.java
@@ -0,0 +1,89 @@
+
+/**
+ * (The Colorable interface) Design an interface named Colorable with a void
+ * method named howToColor(). Every class of a colorable object must implement
+ * the Colorable interface. Design a class named Square that extends
+ * GeometricObject and implements Colorable. Implement howToColor to
+ * display the message Color all four sides.
+ *
+ * Draw a UML diagram that involves Colorable, Square, and GeometricObject.
+ * Write a test program that creates an array of five GeometricObjects. For each
+ * object in the array, display its area and invoke its howToColor method if it
+ * is colorable.
+ *
+ * @author martinfall
+ */
+public abstract class GeometricObject {
+
+    private String color = "white";
+    private boolean filled;
+    private java.util.Date dateCreated;
+
+    /**
+     * Construct a default geometric object
+     */
+    protected GeometricObject() {
+        dateCreated = new java.util.Date();
+    }
+
+    /**
+     * Construct a geometric object with color and filled value
+     */
+    protected GeometricObject(String color, boolean filled) {
+        dateCreated = new java.util.Date();
+        this.color = color;
+        this.filled = filled;
+    }
+
+    /**
+     * Return color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * Set a new color
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * Return filled. Since filled is boolean,
+     * the get method is named isFilled
+     */
+    public boolean isFilled() {
+        return filled;
+    }
+
+    /**
+     * Set a new filled
+     */
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    /**
+     * Get dateCreated
+     */
+    public java.util.Date getDateCreated() {
+        return dateCreated;
+    }
+
+    @Override
+    public String toString() {
+        return "created on " + dateCreated + "\ncolor: " + color
+                + " and filled: " + filled;
+    }
+
+    /**
+     * Abstract method getArea
+     */
+    public abstract double getArea();
+
+    /**
+     * Abstract method getPerimeter
+     */
+    public abstract double getPerimeter();
+}
