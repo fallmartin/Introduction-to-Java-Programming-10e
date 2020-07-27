83208d51552e8abc79469c551d52fe8acbda2bf4
fallmartin <martinfall@gmail.com>
2020-07-27 02:24:49 -0400

    Create Circle.java

---
+++ Chapter 13/Programming Exercise 13.5/Circle.java
@@ -0,0 +1,65 @@
+
+/**
+ *
+ * @author martinfall
+ */
+public class Circle extends GeometricObject {
+
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * Return radius
+     *
+     * @return
+     */
+    public double getRadius() {
+        return radius;
+    }
+
+    /**
+     * Set a new radius
+     *
+     * @param radius
+     */
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    /**
+     * Return area
+     */
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    /**
+     * Return diameter
+     *
+     * @return
+     */
+    public double getDiameter() {
+        return 2 * radius;
+    }
+
+    @Override
+    /**
+     * Return perimeter
+     */
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    /* Print the circle info */
+    public void printCircle() {
+        System.out.println("The circle is created " + getDateCreated()
+                + " and the radius is " + radius);
+    }
+}
