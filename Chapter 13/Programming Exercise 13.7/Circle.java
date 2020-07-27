a6361d4ab2e9817f0366114efb534cb3709d3003
fallmartin <martinfall@gmail.com>
2020-07-27 03:07:00 -0400

    Create Circle.java

---
+++ Chapter 13/Programming Exercise 13.7/Circle.java
@@ -0,0 +1,69 @@
+
+/**
+ * (The Colorable interface) Design an interface named Colorable with a void
+ * method named howToColor(). Every class of a colorable object must implement
+ * the Colorable interface. Design a class named Square that extends
+ * GeometricObject and implements Colorable. Implement howToColor to
+ * display the message Color all four sides.
+ *
+ * Draw a UML diagram that involves Colorable, Square, and GeometricObject.
+ * Write a test program that creates an array of five GeometricObjects. For each
+ * object in the array, display its area and invoke its howToColor method if it
+ * is colorable.
+ *
+ * @author martinfall
+ */
+public class Circle extends GeometricObject {
+
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * Return radius
+     */
+    public double getRadius() {
+        return radius;
+    }
+
+    /**
+     * Set a new radius
+     */
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    /**
+     * Return area
+     */
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    /**
+     * Return diameter
+     */
+    public double getDiameter() {
+        return 2 * radius;
+    }
+
+    @Override
+    /**
+     * Return perimeter
+     */
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    /* Print the circle info */
+    public void printCircle() {
+        System.out.println("The circle is created " + getDateCreated()
+                + " and the radius is " + radius);
+    }
+}
