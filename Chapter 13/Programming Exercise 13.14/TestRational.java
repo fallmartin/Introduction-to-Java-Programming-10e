16db8ebd7bd7163fc04ffaa9112d379fe8b99264
fallmartin <martinfall@gmail.com>
2020-07-28 03:25:52 -0400

    Create TestRational.java

---
+++ Chapter 13/Programming Exercise 13.14/TestRational.java
@@ -0,0 +1,22 @@
+
+/**
+ *
+ * @author martinfall
+ */
+public class TestRational {
+
+    public static void main(String[] args) {
+        // Create a new Rational object
+        Rational r = new Rational(4, 5);
+
+        // A few method calls on members of Rational
+        System.out.println(r + " + " + (new Rational(4, 5) + "")
+                + " = " + (r.add(new Rational(4, 5))));
+        System.out.println(r + " - " + (new Rational(4, 5) + "")
+                + " = " + (r.subtract(new Rational(4, 5))));
+        System.out.println(r + " x " + (new Rational(4, 5) + "")
+                + " = " + (r.multiply(new Rational(4, 5))));
+        System.out.println(r + " ÷ " + (new Rational(4, 5) + "")
+                + " = " + (r.divide(new Rational(4, 5))));
+    }
+}
