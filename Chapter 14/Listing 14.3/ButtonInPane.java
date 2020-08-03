96847b1ed44cb2d4cd1ee6645cabb7cb06c00813
fallmartin <martinfall@gmail.com>
2020-08-03 05:09:02 -0400

    Create ButtonInPane.java

---
+++ Chapter 14/Listing 14.3/ButtonInPane.java
@@ -0,0 +1,28 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+
+/**
+ *
+ * @author martinfall
+ */
+public class ButtonInPane extends Application {
+
+    @Override // Override abstract method start(Stage) in Application
+    public void start(Stage primaryStage) throws Exception {
+        // Create a scene and place a button in the scene
+        StackPane pane = new StackPane();
+        pane.getChildren().add(new Button("OK"));
+        Scene scene = new Scene(pane, 200, 50);
+        primaryStage.setTitle("Button in a pane"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
