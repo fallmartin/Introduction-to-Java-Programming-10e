ba301c1f6394a61bcbde6b3b8dde57f5d38b01c6
fallmartin <martinfall@gmail.com>
2020-08-10 02:55:12 -0400

    Create DisplayClock.java

---
+++ Chapter 14/Listing 14.20/DisplayClock.java
@@ -0,0 +1,40 @@
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+/**
+ *
+ * @author martinfall
+ */
+public class DisplayClock extends Application {
+
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) throws Exception {
+        // Create a clock and a label
+        ClockPane clock = new ClockPane();
+        String timeString = clock.getHour() + ":"
+                + clock.getMinute() + ":"
+                + clock.getSecond();
+        Label lblCurrentTime = new Label(timeString);
+
+        // Place clock and label in border pane
+        BorderPane pane = new BorderPane();
+        pane.setCenter(clock);
+        pane.setBottom(lblCurrentTime);
+        BorderPane.setAlignment(lblCurrentTime, Pos.TOP_CENTER);
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 250, 250);
+        primaryStage.setTitle("DisplayClock"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
