6740e6fd8d4866a5972b7ea7eda399ed1f2852f6
fallmartin <martinfall@gmail.com>
2020-08-23 02:36:03 -0400

    Create ControlCircle.java

---
+++ Chapter 15/Listing 15.3/ControlCircle.java
@@ -0,0 +1,89 @@
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author martinfall
+ */
+public class ControlCircle extends Application {
+
+    private CirclePane circlePane = new CirclePane();
+
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) throws Exception {
+        // Hold two buttons in an HBox
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
+        Button btEnlarge = new Button("Enlarge");
+        Button btShrink = new Button("Shrink");
+        hBox.getChildren().add(btEnlarge);
+        hBox.getChildren().add(btShrink);
+
+        // Create and register the handler
+        btEnlarge.setOnAction(new EnlargeHandler());
+        btShrink.setOnAction(new ShrinkHanlder());
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(circlePane);
+        borderPane.setBottom(hBox);
+        BorderPane.setAlignment(hBox, Pos.CENTER);
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(borderPane, 200, 150);
+        primaryStage.setTitle("ControlCircle");// Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    class EnlargeHandler implements EventHandler<ActionEvent> {
+
+        @Override // Override the handle method of the EventHandler interface
+        public void handle(ActionEvent t) {
+            circlePane.enlarge();
+        }
+    }
+
+    class ShrinkHanlder implements EventHandler<ActionEvent> {
+
+        @Override // Override the handle method of the EventHandler interface
+        public void handle(ActionEvent t) {
+            circlePane.shrink();
+        }
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
+
+class CirclePane extends StackPane {
+
+    Circle circle = new Circle(50);
+
+    public CirclePane() {
+        getChildren().add(circle);
+        circle.setStroke(Color.BLACK);
+        circle.setFill(Color.WHITE);
+    }
+
+    public void enlarge() {
+        circle.setRadius(circle.getRadius() + 2);
+    }
+
+    public void shrink() {
+        circle.setRadius(circle.getRadius() > 2
+                ? circle.getRadius() - 2 : circle.getRadius());
+    }
+}
