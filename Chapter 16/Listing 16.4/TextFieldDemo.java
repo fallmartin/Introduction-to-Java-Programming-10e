8406cf603c2aaa2288f80f465c9944fcb7dd78a0
fallmartin <martinfall@gmail.com>
2020-09-18 03:26:37 -0400

    Create TextFieldDemo.java

---
+++ Chapter 16/Listing 16.4/TextFieldDemo.java
@@ -0,0 +1,48 @@
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author martinfall
+ */
+public class TextFieldDemo extends RadioButtonDemo {
+
+    @Override // Override the getPane method in the super class
+    protected BorderPane getPane() {
+        BorderPane pane = super.getPane();
+
+        BorderPane paneForTextField = new BorderPane();
+        paneForTextField.setPadding(new Insets(5, 5, 5, 5));
+        paneForTextField.setStyle("-fx-border-color: green");
+        paneForTextField.setLeft(new Label("Enter a message: "));
+        
+        TextField tf = new TextField();
+        tf.setAlignment(Pos.BOTTOM_RIGHT);
+        paneForTextField.setCenter(tf);
+        pane.setTop(paneForTextField);
+        
+        tf.setOnAction(e -> {text.setText(tf.getText());});
+
+        return pane;
+    }
+
+    /**
+     * The main method is only needed for the IDE with limited
+     * JavaFX support.Not needed for running from the command line.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
