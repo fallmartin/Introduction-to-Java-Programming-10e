93f8c9dfd689ee5969815aed405f172d19e355a4
fallmartin <martinfall@gmail.com>
2020-09-18 03:28:25 -0400

    Add files via upload

---
+++ Chapter 16/Listing 16.5/RadioButtonDemo.java
@@ -0,0 +1,55 @@
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author martinfall
+ */
+public class RadioButtonDemo extends CheckBoxDemo {
+
+    @Override
+    protected BorderPane getPane() {
+        BorderPane pane = super.getPane();
+
+        VBox paneForRadioButtons = new VBox(20);
+        paneForRadioButtons.setPadding(new Insets(5, 5, 5, 5));
+        paneForRadioButtons.setStyle("-fx-border-color: green");
+        paneForRadioButtons.setStyle("-fx-border_width: 2px; -fx-border-color: green");
+        RadioButton rbRed = new RadioButton("Red");
+        RadioButton rbGreen = new RadioButton("Green");
+        RadioButton rbBlue = new RadioButton("Blue");
+        paneForRadioButtons.getChildren().addAll(rbRed, rbGreen, rbBlue);
+        pane.setLeft(paneForRadioButtons);
+
+        ToggleGroup group = new ToggleGroup();
+        rbRed.setToggleGroup(group);
+        rbGreen.setToggleGroup(group);
+        rbBlue.setToggleGroup(group);
+
+        rbRed.setOnAction(e -> {
+            if (rbRed.isSelected()) {
+                text.setFill(Color.RED);
+            }
+        });
+
+        rbGreen.setOnAction(e -> {
+            if (rbGreen.isSelected()) {
+                text.setFill(Color.GREEN);
+            }
+        });
+
+        rbBlue.setOnAction(e -> {
+            if (rbBlue.isSelected()) {
+                text.setFill(Color.BLUE);
+            }
+        });
+
+        return pane;
+    }
+}
