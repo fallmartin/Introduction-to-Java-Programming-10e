f140287f26a967ad7520b3a0437273dd4f41d02e
Martin Fall <martinfall@gmail.com>
2020-12-04 03:52:57 -0500

    Create IntegerMatrix.java

---
+++ Chapter 19/Listing 19.11/IntegerMatrix.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @author martinfall
+ */
+public class IntegerMatrix extends GenericMatrix<Integer> {
+
+    @Override // Add two integers
+    protected Integer add(Integer o1, Integer o2) {
+        return o1 + o2;
+    }
+
+    @Override // Multiply two integers
+    protected Integer multiply(Integer o1, Integer o2) {
+        return o1 * o2;
+    }
+
+    @Override // Specify zero for an integer
+    protected Integer zero() {
+        return 0;
+    }
+
+}
