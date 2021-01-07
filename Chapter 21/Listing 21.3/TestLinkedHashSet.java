d4856bd6000f7fbf7255090647df4d6e47872418
Martin Fall <martinfall@gmail.com>
2021-01-07 03:40:42 -0500

    Create TestLinkedHashSet.java

---
+++ Chapter 21/Listing 21.3/TestLinkedHashSet.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+/**
+ *
+ * @author martinfall
+ */
+public class TestLinkedHashSet {
+
+    public static void main(String[] args) {
+        // Create a hash set
+        Set<String> set = new HashSet<>();
+
+        // Add strings to the set
+        set.add("London");
+        set.add("Paris");
+        set.add("New York");
+        set.add("San Francisco");
+        set.add("Beijing");
+        set.add("New York");
+
+        System.out.println(set);
+
+        // Display the elements in the hash set
+        for (String element : set) {
+            System.out.print(element.toLowerCase() + " ");
+        }
+    }
+}
