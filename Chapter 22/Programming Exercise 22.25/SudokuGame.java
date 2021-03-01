9487566d3ed866a972645f35e93539f58049f0b3
Martin Fall <martinfall@gmail.com>
2021-03-01 04:13:28 -0500

    Create SudokuGame.java

---
+++ Chapter 22/Programming Exercise 22.25/SudokuGame.java
@@ -0,0 +1,116 @@
+package sudoku;
+
+import java.util.*;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+/**
+ * (Game: Sudoku) The complete solution for the Sudoku problem is given in
+ * Supplement VI.C. Write a program that lets the user enter the input from the
+ * text fields, as shown in Figure 22.15a. Clicking the Solve button displays
+ * the result, as shown in Figure 22.15b–c.
+ *
+ * @author martinfall
+ */
+public class SudokuGame extends Application {
+
+    private ArrayList<Integer[][]> solutions = new ArrayList<>();
+    private int counter = 0;
+
+    @Override // Override the start method in the Application class
+    public void start(Stage stage) throws Exception {
+
+        // Status label
+        Label label = new Label("Label");
+        label.setFont(Font.font(15));
+        label.setAlignment(Pos.CENTER);
+        label.setPadding(new Insets(10));
+
+        // Sudoku grid pane
+        SudokuPane sudoku = new SudokuPane();
+
+        // Buttons for Solve and Clear and the HBox to hold them
+        Button btSolve = new Button("Solve");
+        btSolve.setFont(Font.font(15));
+
+        Button btClear = new Button("Clear");
+        btClear.setFont(Font.font(15));
+
+        Button btNext = new Button("Next");
+        btNext.setFont(Font.font(15));
+
+        HBox paneForButtons = new HBox(5);
+        paneForButtons.getChildren().addAll(btSolve, btClear, btNext);
+        paneForButtons.setAlignment(Pos.CENTER);
+        paneForButtons.setPadding(new Insets(10));
+
+        // Border pane to hold the label, Sudoku board, and buttons pane
+        BorderPane pane = new BorderPane();
+        pane.setTop(label);
+        pane.setCenter(sudoku);
+        pane.setBottom(paneForButtons);
+        BorderPane.setAlignment(label, Pos.CENTER);
+        BorderPane.setAlignment(paneForButtons, Pos.CENTER);
+
+        // Create a new scene and place it in the stage
+        Scene scene = new Scene(pane);
+        stage.setTitle("Sudoku"); // Set the stage title
+        stage.setScene(scene); // Place the scene in the stage
+        stage.show(); // Display the stage
+        stage.setResizable(false); // Make the stage non-resizable
+
+        // Event handler for the Solve button
+        btSolve.setOnAction(e -> {
+            solutions.clear();
+            Integer[][] grid = sudoku.readAPuzzle();
+
+            if (!Sudoku.isValid(grid)) {
+                label.setText("Invalid Input");
+            } else {
+                Sudoku game = new Sudoku(grid);
+                solutions = Sudoku.getSolutions();
+
+                if (solutions.size() > 0) {
+                    label.setText((counter + 1) + "/" + solutions.size()
+                            + " solutions");
+                    sudoku.paint(solutions.get(counter));
+                } else {
+                    label.setText("No solutions found");
+                }
+            }
+        });
+
+        // Event handler for the Clear button
+        btClear.setOnAction(e -> {
+            solutions.clear();
+            sudoku.clear();
+            label.setText("");
+        });
+
+        // Event handler for the Next button
+        btNext.setOnAction(e -> {
+            if (counter < solutions.size() - 1) {
+                counter++;
+            }
+
+            sudoku.paint(solutions.get(counter));
+        });
+    }
+
+    /**
+     * Main method with driver code.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
