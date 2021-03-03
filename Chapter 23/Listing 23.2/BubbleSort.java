61674e880b9a1594d0eb16f7276af8d183ef513c
Martin Fall <martinfall@gmail.com>
2021-03-03 03:44:15 -0500

    Create BubbleSort.java

---
+++ Chapter 23/Listing 23.2/BubbleSort.java
@@ -0,0 +1,43 @@
+package listing_23_2;
+
+/**
+ *
+ * @author martinfall
+ */
+public class BubbleSort {
+
+    public static void bubbleSort(int[] list) {
+        for (int k = 1; k < list.length; k++) {
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void bubbleSort2(int[] list) {
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length - i - 1; j++) {
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+    }
+}
