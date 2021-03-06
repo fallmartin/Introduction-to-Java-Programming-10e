282e4a302182b45103d025fd08e4c75e6ed94649
Martin Fall <martinfall@gmail.com>
2021-03-06 04:15:21 -0500

    Create QuickSort.java

---
+++ Chapter 23/Listing 23.8/QuickSort.java
@@ -0,0 +1,90 @@
+package listing_23_8;
+
+/**
+ * Output:
+ * List contents: -2 1 2 2 3 3 5 6 12 14
+ *
+ * @author martinfall
+ */
+public class QuickSort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+        quickSort(list);
+        printList(list);
+    }
+
+    public static void quickSort(int[] list) {
+        quickSort(list, 0, list.length - 1);
+    }
+
+    public static void quickSort(int[] list, int first, int last) {
+        if (last > first) {
+            int pivotIndex = partition(list, first, last);
+            quickSort(list, 0, pivotIndex - 1);
+            quickSort(list, pivotIndex + 1, last);
+        }
+    }
+
+    /**
+     * Partition the array list[first..last].
+     *
+     * @param list
+     * @param first
+     * @param last
+     * @return
+     */
+    public static int partition(int[] list, int first, int last) {
+        int pivot = list[first]; // Choose the first element as the pivot
+        int low = first + 1; // Index for forward search
+        int high = last; // Index for backward search
+
+        while (high > low) {
+            // Search forward for the left
+            while (low <= high && list[low] <= pivot) {
+                low++;
+            }
+
+            // Search backward from the right
+            while (low <= high && list[high] > pivot) {
+                high--;
+            }
+
+            // Swap two elements in the list
+            if (high > low) {
+                int temp = list[high];
+                list[high] = list[low];
+                list[low] = temp;
+            }
+        }
+
+        while (high > first && list[high] >= pivot) {
+            high--;
+        }
+
+        // Swap pivot with list high
+        if (pivot > list[high]) {
+            list[first] = list[high];
+            list[high] = pivot;
+            return high;
+        } else {
+            return first;
+        }
+    }
+
+    /**
+     * Print the contents of the list to the console.
+     *
+     * @param list
+     */
+    public static void printList(int[] list) {
+        System.out.print("List contents: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+    }
+}
